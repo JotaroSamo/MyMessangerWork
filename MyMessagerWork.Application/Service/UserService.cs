b4ff33fb2ebbcf6b7f65ed23f352b82d0e66f3cf
Dimension-W <Dimension-W@HOME-PC>
2024-08-20 15:05:33 +0300

    save

--- MyMessagerWork.Application/Service/UserService.cs
+++ MyMessagerWork.Application/Service/UserService.cs
@@ -1,4 +1,5 @@
-﻿using MyMessagerWork.Core.Model;
+﻿using MyMessagerWork.Core.Abstract;
+using MyMessagerWork.Core.Model;
 using MyMessagerWork.DataAcess.Repositories;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyMessagerWork.Application.Service
 {
-    public class UserService
+    public class UserService: IUserService
     {
         private readonly IRepositoryUser _repository;
 
@@ -16,10 +17,38 @@ namespace MyMessagerWork.Application.Service
         {
             _repository = repository;
         }
-        public async Task<Guid> AddUser(User entity)
+
+        public async Task<Guid> AddAsyncUser(User entity)
         {
-            await _repository.AddAsync( entity );
-            return entity.Id;
+           return await _repository.AddAsync(entity);
+          
+        }
+
+
+        public async Task<Guid> DeleteByIdUser(Guid id)
+        {
+          return  await _repository.DeleteById(id);
+            
+        }
+
+        public async Task<List<User>> GetAllListUserAsync()
+        {
+           return await _repository.GetAllListAsync();
+        }
+
+        public async Task<User> GetByIdUserAsync(Guid id)
+        {
+          return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task SaveAsyncUser()
+        {
+           await _repository.SaveAsync();
+        }
+
+        public async Task<Guid> UpdateUser(User entity)
+        {
+           return await _repository.Update(entity);
         }
     }
 }
---
+++ MyMessagerWork.Core/Abstract/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using MyMessagerWork.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessagerWork.Core.Abstract
+{
+    public interface IUserService
+    {
+        Task<Guid> AddAsyncUser(User entity);
+        Task<Guid> DeleteByIdUser(Guid id);
+        Task<List<User>> GetAllListUserAsync();
+        Task<User> GetByIdUserAsync(Guid id);
+        Task<Guid> UpdateUser(User entity);
+        //Task<IQueryable<User>> AsQueryable();
+        Task SaveAsyncUser();
+    }
+}
