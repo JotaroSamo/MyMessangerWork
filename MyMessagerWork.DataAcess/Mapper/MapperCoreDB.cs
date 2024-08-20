b0210eb033b26ea2fda2d4bd34d56d7d0637a1bf
Dimension-W <Dimension-W@HOME-PC>
2024-08-20 14:52:50 +0300

    save

--- MyMessagerWork.DataAcess/Mapper/MapperCoreDB.cs
+++ MyMessagerWork.DataAcess/Mapper/MapperCoreDB.cs
@@ -4,6 +4,7 @@ using MyMessagerWork.DataAcess.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,9 @@ namespace MyMessagerWork.DataAcess.Mapper
         public MapperCoreDB()
         {
             CreateMap<User, UserEntity>().ReverseMap();
+            CreateMap<Chat, ChatEntity>().ReverseMap();
+            CreateMap<ChatUser, ChatUserEntity>().ReverseMap();
+            CreateMap<Message, MessageEntity>().ReverseMap();
         }
     }
 }
